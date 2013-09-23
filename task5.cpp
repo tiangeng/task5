321361599117c3b0292109016828977a028d95ac
tiangeng <1187684319@qq.com>
2013-09-23 08:27:46 +0800

    C++第二次作业第5题

---
+++ task5.cpp
@@ -0,0 +1,25 @@
+// task5.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+using namespace std;
+int count(int n){
+int t=1,s;
+if(n==1)
+s=1;
+else
+s=n*count(n-1);
+return s;}
+void main(){
+int m,n,i,p=1,g;
+cin>>m>>n;
+for(i=1;i<=n;i++){
+p=m*p;
+--m;
+}
+g=p/count(n);
+cout<<g<<endl;
+}
